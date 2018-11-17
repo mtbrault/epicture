cbcb92907e84ca3bf5e8f037af2e0c0404e1eb3d
cicorias <zakaria.laabid@epitech.eu>
2018-11-17 01:21:28 +0100

    Android folder and gradlew done

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp2f9253477f514834aa875136339fce38://192.168.0.13:19000";
+
+}
